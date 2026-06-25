51eb21731f8e21d05ceec6f10d280d01b1a0bdbb
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-25 13:43:19 -0300

    Add fonts that will be used in the project

--- Poções e soluções/pocoes-e-solucoes/src/components/Header.tsx
+++ Poções e soluções/pocoes-e-solucoes/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Header() {
     return (
-        <div className="container mx-auto flex justify-center">
+        <div className=" p-8 container mx-auto flex justify-center">
             <ul className="flex gap-8 text-white">
                 <Link to="/"><li>HOME</li></Link>
                 <Link to="/sobre-nos"><li>SOBRE NÓS</li></Link>
