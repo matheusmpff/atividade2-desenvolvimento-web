bce74058a06258961fed403849711f9ffd8c248a
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-25 13:26:03 -0300

    Create header and color thee in index.css

--- Poções e soluções/pocoes-e-solucoes/src/App.jsx
+++ Poções e soluções/pocoes-e-solucoes/src/App.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Produtos from './pages/Produtos'
 import Sobre_nos from './pages/Sobre_nos'
+import Header from './components/Header'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/sobre-nos' element={<Sobre_nos/>}></Route>
-        <Route path='/produtos' element={<Produtos/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className='bg-background min-h-screen'>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/sobre-nos' element={<Sobre_nos />}></Route>
+          <Route path='/produtos' element={<Produtos />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+
   )
 }
 
--- Poções e soluções/pocoes-e-solucoes/src/components/Header.tsx
+++ Poções e soluções/pocoes-e-solucoes/src/components/Header.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 export default function Header() {
     return (
-        <div>
-            <ul>
-                <li>HOME</li>
-                <li>SOBRE NÓS</li>
-                <li>PRODUTOS</li>
+        <div className="container mx-auto flex justify-center">
+            <ul className="flex gap-8 text-white">
+                <Link to="/"><li>HOME</li></Link>
+                <Link to="/sobre-nos"><li>SOBRE NÓS</li></Link>
+                <Link to="/produtos"><li>PRODUTOS</li></Link>
             </ul>
         </div>
     );
