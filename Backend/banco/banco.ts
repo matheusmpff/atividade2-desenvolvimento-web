ec1f82a1dfc375adeb2108e4c299bbd4e03a9d1a
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-25 21:52:43 -0300

    Adjust to initial images to be in uploads folder

--- Backend/banco/banco.ts
+++ Backend/banco/banco.ts
@@ -42,7 +42,7 @@ export async function createProduct({ nome, valor, descricao, imagem }: CREATEPR
         nome,
         valor,
         descricao,
-        imagem
+        imagem: `uploads/${imagem}`
     })
 }
 
@@ -57,37 +57,37 @@ export async function putData() {
         nome: "Poção Blue Sky",
         valor: 300,
         descricao: "Essa poção provê um surto de inspiração por 24 horas. Foi utilizada por John Lennon quando escreveu Lucy in the Sky with Diamonds.",
-        imagem: "initData/rsz-sky.png"
+        imagem: "rsz-sky.png"
     })
     await createProduct({
         nome: "Poção do Perfume Misterioso",
         valor: 200,
         descricao: "Essa poção faz com que você fique cheirando lilás e groselha por 24 dias. Essência muito admirada pelos bruxos.",
-        imagem: "initData/rsz-lilas.png"
+        imagem: "rsz-lilas.png"
     })
     await createProduct({
         nome: "Poção de Pinus",
         valor: 3000,
         descricao: "Essa poção faz com que você fique 10 cm mais alto! Observação: efeitos colaterais desconhecidos",
-        imagem: "initData/rsz-pinus.png"
+        imagem: "rsz-pinus.png"
     })
     await createProduct({
         nome: "Poção da Beleza Eterna",
         valor: 3000,
         descricao: "Veneno que mata rápido.",
-        imagem: "initData/rsz-1beleza.png"
+        imagem: "rsz-1beleza.png"
     })
     await createProduct({
         nome: "Poção do Arco Íro",
         valor: 120,
         descricao: "Traz felicidade momentânea. Pode durar de 10 minutos a 2 dias.",
-        imagem: "initData/rsz-2unicornio.png"
+        imagem: "rsz-2unicornio.png"
     })
     await createProduct({
         nome: "Caldeirão das Verdades Secretas",
         valor: 150,
         descricao: "As pessoas lhe dirão apenas verdades por 1 hora. É necessário beber os 5L.",
-        imagem: "initData/rsz-verdades.png"
+        imagem: "rsz-verdades.png"
     })
 
 }
@@ -95,6 +95,6 @@ export async function putData() {
 export async function DBconnect() {
     await sequelize.sync();
     await Produto.sync();
-    await putData
+    await putData();
     console.log("Conectado com o banco")
 }
